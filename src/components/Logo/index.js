96e8a0623e148623d5e4badd5875e30b3082ff15
PhantomDraven <kevin.statua@gmail.com>
2019-01-31 11:01:22 +0100

    Logo Component

---
+++ src/components/Logo/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import logo from './logo.svg';
+import './style.css';
+
+const LogoRender = () => {
+    return (
+        <div className="App-logo-wrapper">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+    );
+}
+
+export default LogoRender; 
