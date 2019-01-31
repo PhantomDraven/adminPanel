c37456adcc3acf0dd2bf3625ce3a46dc29858932
PhantomDraven <kevin.statua@gmail.com>
2019-01-31 11:01:07 +0100

    React base structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import LogoRender from './components/Logo';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+            <LogoRender />
+        </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+/** Removed due to test environment */
+// import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+/** Removed due to test environment */
+// serviceWorker.register();
