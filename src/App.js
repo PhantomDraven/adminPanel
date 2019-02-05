abe473a1bb95900994d9a5e8065e5681e8e2c655
PhantomDraven <kevin.statua@gmail.com>
2019-02-05 10:09:37 +0100

    Redirect to login if unlogged

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
 
+import { withRouter } from 'react-router-dom';
+import { withFirebase } from './firebase';
+
 import Logo from './components/Logo';
 import Navigation from './components/Navigation';
+
+const APPState = {
+    secure: false,
+}
 
 class App extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {...APPState}
     }
 
     componentWillMount() {
         document.title = this.props.title;
     }
 
+    componentDidMount() {
+        console.log(this.props.firebase.auth.user);
+    }
+
     render() {
         return (
-            <div className="App">
-                <Logo />
-                <Navigation />
-            </div>
+            <React.Fragment>
+                <header className="header">
+                    <Logo />
+                    <Navigation />
+                </header>
+                <main className="main">
+                    { this.props.children }
+                </main>
+                <footer className="footer">
+                </footer>
+            </React.Fragment>
         );
     }
 }
 
-export default App;
+const AppRender = withFirebase(App);
+
+export default withRouter(AppRender);
