c7db166b050e82b9ea3ddefc4eeea7aea0442273
stefano-tonanni <stefano.tonanni@gmail.com>
2019-02-05 11:47:42 +0100

    [FIX ISSUE] [Navigation] Style #8

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import ROUTES from '../../pages/routes';
+import './style.css';
 
 class Navigation extends Component {
     render() {
         return (
-            <nav className="navigation">
-                <Link to="/">Home</Link>
+            <nav className="navigation main-nav">
+              <ul>
+                <li>
+                  <Link to={ROUTES.HOME}>Home</Link>
+                </li>
+                <li>
+                  <Link to={ROUTES.LOGIN}>Login</Link>                
+                </li>
+              </ul>
             </nav>
         );
     }
